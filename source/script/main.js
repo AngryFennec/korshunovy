4a22d3bceb2f8352b86543262eab02024781bf19
Angry Fennec <angryfennecus@gmail.com>
2018-08-25 18:54:12 +0300

    Меню

--- source/script/main.js
+++ source/script/main.js
@@ -1,11 +1,11 @@
 var burger = document.querySelector(".menu-button");
-var menu = document.querySelector(".page-navigation");
+var menu = document.querySelector(".nav-container");
 
-menu.classList.add("page-navigation--close");
+menu.classList.add("nav-container--close");
 burger.classList.add("menu-button--close");
 
 burger.addEventListener ("click", function (evt) {
   evt.preventDefault();
-  menu.classList.toggle("page-navigation--close");
+  menu.classList.toggle("nav-container--close");
   burger.classList.toggle("menu-button--close");
 });
